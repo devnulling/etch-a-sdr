aa7b80c184c9c59f9ca3ad143e82008ee44cf51e
devnulling <devnulling@users.noreply.github.com>
2015-09-29 21:11:12 -0700

    updated arduino code

--- arduino/etch-a-sdr.ino
+++ arduino/etch-a-sdr.ino
@@ -660,20 +660,20 @@ void setup() {
     //
 
     error = MPU6050_read(MPU6050_WHO_AM_I, &c, 1);
-    Serial.print(F("WHO_AM_I : "));
-    Serial.print(c, HEX);
-    Serial.print(F(", error = "));
-    Serial.println(error, DEC);
+//    Serial.print(F("WHO_AM_I : "));
+//    Serial.print(c, HEX);
+//    Serial.print(F(", error = "));
+//    Serial.println(error, DEC);
 
     // According to the datasheet, the 'sleep' bit
     // should read a '1'.
     // That bit has to be cleared, since the sensor
     // is in sleep mode at power-up. 
     error = MPU6050_read(MPU6050_PWR_MGMT_1, &c, 1);
-    Serial.print(F("PWR_MGMT_1 : "));
-    Serial.print(c, HEX);
-    Serial.print(F(", error = "));
-    Serial.println(error, DEC);
+//    Serial.print(F("PWR_MGMT_1 : "));
+//    Serial.print(c, HEX);
+//    Serial.print(F(", error = "));
+//    Serial.println(error, DEC);
 
 
     // Clear the 'sleep' bit to start the sensor.
@@ -783,8 +783,10 @@ void loop() {
     // there is no filter enabled, and the values
     // are not very stable.
     error = MPU6050_read(MPU6050_ACCEL_XOUT_H, (uint8_t *) & accel_t_gyro, sizeof (accel_t_gyro));
-    Serial.print(F("mpu_error = "));
-    Serial.println(error, DEC);
+
+
+//    Serial.print(F("mpu_error = "));
+//    Serial.println(error, DEC);
 
 
     // Swap all high and low bytes.
@@ -805,7 +807,7 @@ void loop() {
 
     // Print the raw acceleration values
 
-    Serial.print(F("mpu_accel: "));
+    Serial.print(F("MPU, "));
     Serial.print(accel_t_gyro.value.x_accel, DEC);
     Serial.print(F(", "));
     Serial.print(accel_t_gyro.value.y_accel, DEC);
@@ -820,25 +822,25 @@ void loop() {
     //   340 per degrees Celsius, -512 at 35 degrees.
     // At 0 degrees: -512 - (340 * 35) = -12412
 
-    //    Serial.print(F("mpu_temp: "));
-    //    dT = ((double) accel_t_gyro.value.temperature + 12412.0) / 340.0;
-    //    Serial.print(dT, 3);
-    //    Serial.println(F(""));
+//    Serial.print(F("mpu_temp: "));
+//    dT = ((double) accel_t_gyro.value.temperature + 12412.0) / 340.0;
+//    Serial.print(dT, 3);
+//    Serial.println(F(""));
 
 
     // Print the raw gyro values.
 
-    //    Serial.print(F("mpu_gyro: "));
-    //    Serial.print(accel_t_gyro.value.x_gyro, DEC);
-    //    Serial.print(F(", "));
-    //    Serial.print(accel_t_gyro.value.y_gyro, DEC);
-    //    Serial.print(F(", "));
-    //    Serial.print(accel_t_gyro.value.z_gyro, DEC);
-    //    Serial.print(F(", "));
-    //    Serial.println(F(""));
+//    Serial.print(F("mpu_gyro: "));
+//    Serial.print(accel_t_gyro.value.x_gyro, DEC);
+//    Serial.print(F(", "));
+//    Serial.print(accel_t_gyro.value.y_gyro, DEC);
+//    Serial.print(F(", "));
+//    Serial.print(accel_t_gyro.value.z_gyro, DEC);
+//    Serial.print(F(", "));
+//    Serial.println(F(""));
 
     //delay(200);
-    delay(250);
+   delay(100);
 
 
     // if a character is sent from the serial monitor,
